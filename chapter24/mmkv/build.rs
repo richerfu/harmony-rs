fc52350bb876e69bfc6fa86f794f15901dfed53b
southorange0929 <southorange0929@foxmail.com>
2024-02-19 18:05:44 +0800

    fix: mmkv build failed

--- chapter24/mmkv/build.rs
+++ chapter24/mmkv/build.rs
@@ -2,7 +2,6 @@ use cc::Build;
 use napi_build_ohos;
 
 fn main() {
-    std::env::set_var("CXX","/Volumes/PSSD/code/harmony/harmony-rs/chapter24/mmkv/scripts/aarch64-unknown-linux-ohos-clang++.sh");
     Build::new()
         .file("mmkv/Core/MMKV.cpp")
         .debug(true)
