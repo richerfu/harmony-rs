79b546fbf787935358553bf47fac14bfbfb2411f
southorange0929 <southorange0929@foxmail.com>
2024-02-29 09:43:24 +0800

    refactor: update task

--- chapter24/fib/src/lib.rs
+++ chapter24/fib/src/lib.rs
@@ -1,46 +1,39 @@
-use napi_ohos::{CallContext, Env, JsNumber, JsObject, Result, Task};
-use napi_derive_ohos::{js_function, module_exports};
+use napi_derive_ohos::napi;
+use napi_ohos::{Env, JsNumber, JsObject, Result, Task};
 
 struct ComputeFib {
-  n: u32,
+    n: u32,
 }
 
-
 fn fibonacci_native(n: u32) -> u32 {
-  match n {
-    1 | 2 => 1,
-    _ => fibonacci_native(n - 1) + fibonacci_native(n - 2),
-  }
+    match n {
+        1 | 2 => 1,
+        _ => fibonacci_native(n - 1) + fibonacci_native(n - 2),
+    }
 }
 
 impl ComputeFib {
-  pub fn new(n: u32) -> ComputeFib {
-    ComputeFib { n }
-  }
+    pub fn new(n: u32) -> ComputeFib {
+        ComputeFib { n }
+    }
 }
 
 impl Task for ComputeFib {
-  type Output = u32;
-  type JsValue = JsNumber;
+    type Output = u32;
+    type JsValue = JsNumber;
 
-  fn compute(&mut self) -> Result<Self::Output> {
-    Ok(fibonacci_native(self.n))
-  }
+    fn compute(&mut self) -> Result<Self::Output> {
+        Ok(fibonacci_native(self.n))
+    }
 
-  fn resolve(&mut self, env: Env, output: Self::Output) -> Result<Self::JsValue> {
-    env.create_uint32(output)
-  }
+    fn resolve(&mut self, env: Env, output: Self::Output) -> Result<Self::JsValue> {
+        env.create_uint32(output)
+    }
 }
 
-#[js_function(0)]
-pub fn native_fib(ctx: CallContext) -> Result<JsObject> {
-  let task = ComputeFib::new(45);
-  let async_promise = ctx.env.spawn(task).unwrap();
-  Ok(async_promise.promise_object())
-}
-
-#[module_exports]
-pub fn register_js(mut exports: JsObject) -> Result<()> {
-  exports.create_named_method("fib", native_fib).unwrap();
-  Ok(())
+#[napi(ts_return_type="Promise<number>")]
+pub fn fib(env: Env, init: u32) -> Result<JsObject> {
+    let task = ComputeFib::new(init);
+    let async_promise = env.spawn(task).unwrap();
+    Ok(async_promise.promise_object())
 }
